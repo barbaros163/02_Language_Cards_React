7db99d39f3f9dc1e1b8e0b50f63c5d2300ee6ec6
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-09 19:42:01 +0100

    card components added

---
+++ rc07_language-cards/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import reactLogo from "../assets/react.svg";
+
+const Header = () => {
+  return (
+    <Container>
+      <Image src={reactLogo} width="400px"></Image>
+    </Container>
+  );
+};
+
+export default Header;

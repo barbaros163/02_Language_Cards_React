7982da41df5999b38df841d380d63d762902ac3a
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-09 23:19:30 +0100

    breakpoints added

--- rc07_language-cards/src/components/Card.jsx
+++ rc07_language-cards/src/components/Card.jsx
@@ -1,15 +1,22 @@
 import Language from "./Language";
 import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { data } from "../helpers/data";
 
 const Card = () => {
   return (
-    <Container className="rounded-4 mt-4" style={{ background: "#f48b29" }}>
-      <h1>Languages</h1>
-      {data.map((lang, index) => {
-        return <Language {...lang} key={index} />;
-        // return <Language lang={lang} />
-      })}
+    <Container className="rounded-4 mt-4 p-4" style={{ background: "#f48b29" }}>
+      <h1 className="text-light my-2">Languages</h1>
+      <Row className="g-3 justify-content-center">
+        {data.map((lang, index) => {
+          return (
+            <Col sm={6} md={4} lg={3} key={index}>
+              <Language {...lang} />
+            </Col>
+          );
+        })}
+      </Row>
     </Container>
   );
 };
--- rc07_language-cards/src/components/Language.jsx
+++ rc07_language-cards/src/components/Language.jsx
@@ -1,5 +1,15 @@
-const Language = ({ lang }) => {
-  return <div>Language</div>;
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+
+const Language = ({ name, img, options }) => {
+  return (
+    <Container style={{ background: "peachpuff" }} className="p-4 rounded-2">
+      <Container>
+        <Image src={img} width="70%"></Image>
+        <h3 className="display-6">{name}</h3>
+      </Container>
+    </Container>
+  );
 };
 
 export default Language;
